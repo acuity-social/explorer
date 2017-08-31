f4d32bcd1f2125fa91ca2dea62615ba355da5650
Jonathan Brown <jbrown@link-blockchain.org>
2017-08-31 18:54:38 +0700

    Use new HTTPS RPC.

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ angular.module('ethExplorer', ['ngRoute','ui.bootstrap'])
     }])
     .run(function($rootScope) {
         var web3 = new Web3();
-        var eth_node_url = 'http://139.162.15.124:8645'; // TODO: remote URL
+        var eth_node_url = 'https://rpc.link-blockchain.org:8647';
 	web3.setProvider(new web3.providers.HttpProvider(eth_node_url));
         $rootScope.web3 = web3;
         function sleepFor( sleepDuration ){
