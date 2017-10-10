1af4d7c55c5cba094ef2c3669680b5f5af70fec0
Jonathan Brown <jbrown@link-blockchain.org>
2017-10-10 11:55:54 +0700

    Switch to blocks.mix-blockchain.org

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ angular.module('ethExplorer', ['ngRoute','ui.bootstrap'])
     }])
     .run(function($rootScope) {
         var web3 = new Web3();
-        var eth_node_url = 'https://rpc.mixchain.tech:8647';
+        var eth_node_url = 'https://rpc.mix-blockchain.org:8647';
 	web3.setProvider(new web3.providers.HttpProvider(eth_node_url));
         $rootScope.web3 = web3;
         function sleepFor( sleepDuration ){
